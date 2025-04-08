690cdfbcda031fbfbbf758934e38bc88c8b2cb17
murloc-craft <lysdora.web@gmail.com>
2025-04-08 08:11:47 +0200

    Facing Direction

--- hero/hero.gd
+++ hero/hero.gd
@@ -1,10 +1,24 @@
 extends CharacterBody2D
 
+const SIDE_BIAS: = 0.1
+
+var facing_direction: = Vector2.DOWN :
+	set(value):
+		if value == Vector2.ZERO: return
+		value = value.normalized()
+		if abs(value.x) >= abs(value.y) - SIDE_BIAS:
+			value = Vector2(sign(value.x), 0)
+		else:
+			value = Vector2(0, sign(value.y))
+		facing_direction = value
+		print(facing_direction)
+
 func _ready() -> void:
 	motion_mode = MOTION_MODE_FLOATING
 
 func _physics_process(_delta: float) -> void:
 	var input_vector = Input.get_vector("left", "right", "up", "down")
+	facing_direction = input_vector
 	velocity = input_vector * 100
 	move_and_slide()
 	
