0fff8f99bc47ac08c7ccdb4fed7c3f9e373c7360
murloc-craft <lysdora.web@gmail.com>
2025-04-08 07:31:13 +0200

    Ajout des animations Idle et Run avec AnimationPlayer

--- hero/hero.gd
+++ hero/hero.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 func _ready() -> void:
 	motion_mode = MOTION_MODE_FLOATING
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var input_vector = Input.get_vector("left", "right", "up", "down")
 	velocity = input_vector * 100
 	move_and_slide()
