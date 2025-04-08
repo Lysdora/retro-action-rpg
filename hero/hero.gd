b49039248010dbf54fe1d41becf257877550b611
murloc-craft <lysdora.web@gmail.com>
2025-04-08 07:15:37 +0200

    Creation du Hero/Mouvement

---
+++ hero/hero.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+func _ready() -> void:
+	motion_mode = MOTION_MODE_FLOATING
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Input.get_vector("left", "right", "up", "down")
+	velocity = input_vector * 100
+	move_and_slide()
+	
